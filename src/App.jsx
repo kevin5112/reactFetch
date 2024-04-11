8820e78ad53707ffee6fdb62b12e6f0b7225ec5b
kevin5112 <kevin5112@yahoo.com>
2024-04-11 14:13:23 -0700

    use fetch and simulate delay with loading and errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const useImageURL = () => {
+  const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/photos', { mode: 'cors' })
+        .then((response) => {
+          if (response.status >= 400) {
+            throw new Error('server error');
+          }
+          return response.json();
+        })
+        .then((response) => setImageURL(response[0].url))
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
+    }, 1000);
+  }, []);
+
+  return { imageURL, error, loading };
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { imageURL, error, loading } = useImageURL();
+
+  if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    imageURL && (
+      <>
+        <h1>An image</h1>
+        <img src={imageURL} alt={'placeholder text'} />
+      </>
+    )
+  );
 }
 
-export default App
+export default App;
